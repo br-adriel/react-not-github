880c86a74f35a3aa583b11214437237b536f2b45
Adriel <adriel.fsantos@outlook.com>
2023-01-05 18:57:43 -0300

    Adiciona cores a theme #2

--- src/components/GlobalStyle.tsx
+++ src/components/GlobalStyle.tsx
@@ -6,20 +6,38 @@ export const darkTheme: Theme = {
   text: '#ffffff',
   bgHover: '#25303B',
   accent: '#1D9BF0',
+  textAlt: '#8B98A5',
+  retweet: '#00BA7C',
+  like: '#F91880',
+  accentHover: 'rgba(29, 155, 240, 0.2)',
+  likeHover: 'rgba(249, 24, 128, 0.2)',
+  retweetHover: 'rgba(0, 186, 124, 0.2)',
 };
 
 export const darkerTheme: Theme = {
+  accentHover: 'rgba(29, 155, 240, 0.2)',
+  likeHover: 'rgba(249, 24, 128, 0.2)',
+  retweetHover: 'rgba(0, 186, 124, 0.2)',
   bg: '#000000',
   text: '#ffffff',
+  textAlt: '#71767B',
   bgHover: '#101010',
   accent: '#1D9BF0',
+  retweet: '#00BA7C',
+  like: '#F91880',
 };
 
 export const lightTheme: Theme = {
+  accentHover: 'rgba(29, 155, 240, 0.2)',
+  likeHover: 'rgba(249, 24, 128, 0.2)',
+  retweetHover: 'rgba(0, 186, 124, 0.2)',
+  textAlt: '#536471',
   bg: '#ffffff',
   text: '#000000',
   bgHover: '#efefef',
   accent: '#1D9BF0',
+  retweet: '#00BA7C',
+  like: '#F91880',
 };
 
 const GlobalStyle = createGlobalStyle`
--- src/global/types.ts
+++ src/global/types.ts
@@ -1,6 +1,12 @@
 export type Theme = {
   bg: string;
   text: string;
+  textAlt: string;
   bgHover: string;
   accent: string;
+  like: string;
+  retweet: string;
+  accentHover: string;
+  likeHover: string;
+  retweetHover: string;
 };
