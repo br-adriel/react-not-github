fb30ff2439fb404b3f3543c27e0d376ce5a8ccf9
Adriel <adriel.fsantos@outlook.com>
2023-01-10 20:31:43 -0300

    Adiciona exportação de sub páginas de Notification #6

--- src/pages/Notifications/index.ts
+++ src/pages/Notifications/index.ts
@@ -1,5 +1,7 @@
 import AllNotifications from './AllNotifications';
+import Mentions from './Mentions';
 import Notifications from './Notifications';
+import Verified from './Verified';
 
 export default Notifications;
-export { AllNotifications };
+export { AllNotifications, Mentions, Verified };
