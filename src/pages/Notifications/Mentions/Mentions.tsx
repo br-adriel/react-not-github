a36a73b0cfe7e9e71fd5631e68a3ed146ed29451
Adriel <adriel.fsantos@outlook.com>
2023-01-10 19:40:46 -0300

    Adiciona sub página Mentions #6

---
+++ src/pages/Notifications/Mentions/Mentions.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Notification from '../../../components/Notification';
+
+const Mentions = () => {
+  return (
+    <>
+      <Notification type='response' />
+      <Notification type='response' />
+      <Notification type='response' />
+      <Notification type='response' />
+      <Notification type='response' />
+      <Notification type='response' />
+    </>
+  );
+};
+
+export default Mentions;
