a49332f3bcf17608fafac0f71797593ebb931ab8
Adriel <adriel.fsantos@outlook.com>
2023-01-10 19:38:09 -0300

    Adiciona sub página AllNotifications #6

---
+++ src/pages/Notifications/AllNotifications/AllNotifications.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Notification from '../../../components/Notification';
+
+const AllNotifications = () => {
+  return (
+    <>
+      <Notification type='notification' />
+      <Notification type='like' />
+      <Notification type='device' />
+      <Notification type='follow' />
+      <Notification type='response' />
+      <Notification type='like' />
+      <Notification type='like' />
+    </>
+  );
+};
+
+export default AllNotifications;
