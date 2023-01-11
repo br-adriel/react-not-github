7bc99bdc51da4df0d42ab6fc192ea8e8a2fe9118
Adriel <adriel.fsantos@outlook.com>
2023-01-11 16:40:34 -0300

    Limita áreas da navbar de acordo com autenticação #11

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,20 @@
+import { useContext } from 'react';
 import { AiFillBell, AiOutlineBell } from 'react-icons/ai';
 import { BiHash, BiSearch } from 'react-icons/bi';
 import { BsPerson, BsPersonFill } from 'react-icons/bs';
 import { HiMail, HiOutlineMail } from 'react-icons/hi';
 import { RiHome7Fill, RiHome7Line } from 'react-icons/ri';
+import AuthGoogleContext from '../../contexts/AuthGoogleContext';
 import NavItem from './NavItem';
 import * as S from './style';
 
 const Navbar = () => {
+  const { user } = useContext(AuthGoogleContext);
   return (
     <S.NavTag>
-      <NavItem icon={<RiHome7Line />} iconActive={<RiHome7Fill />} link='/' />
+      {!user ? null : (
+        <NavItem icon={<RiHome7Line />} iconActive={<RiHome7Fill />} link='/' />
+      )}
       <NavItem
         icon={<BiHash />}
         iconActive={<BiHash style={{ strokeWidth: '1px' }} />}
@@ -22,21 +27,25 @@ const Navbar = () => {
         link='/explore'
         hideLarge={true}
       />
-      <NavItem
-        icon={<AiOutlineBell />}
-        iconActive={<AiFillBell />}
-        link='/notifications'
-      />
-      <NavItem
-        icon={<HiOutlineMail />}
-        iconActive={<HiMail />}
-        link='/messages'
-      />
-      <NavItem
-        icon={<BsPerson />}
-        iconActive={<BsPersonFill />}
-        link='/username'
-      />
+      {!user ? null : (
+        <>
+          <NavItem
+            icon={<AiOutlineBell />}
+            iconActive={<AiFillBell />}
+            link='/notifications'
+          />
+          <NavItem
+            icon={<HiOutlineMail />}
+            iconActive={<HiMail />}
+            link='/messages'
+          />
+          <NavItem
+            icon={<BsPerson />}
+            iconActive={<BsPersonFill />}
+            link='/username'
+          />
+        </>
+      )}
     </S.NavTag>
   );
 };
