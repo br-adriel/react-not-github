5445549c1706d9cd84964bea116018f801ff823b
Adriel <adriel.fsantos@outlook.com>
2023-01-11 16:39:15 -0300

    Implementa contexto de autenticação com google #10

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import GlobalStyle from './components/GlobalStyle';
+import { AuthGoogleProvider } from './contexts/AuthGoogleContext';
 import { Provider } from './contexts/ThemeContext';
 import AppRouter from './routes/AppRouter';
 
 function App() {
   return (
     <Provider>
-      <GlobalStyle />
-      <AppRouter />
+      <AuthGoogleProvider>
+        <GlobalStyle />
+        <AppRouter />
+      </AuthGoogleProvider>
     </Provider>
   );
 }
