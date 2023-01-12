3d0a1c82136059129cc3c267751aec7f12388517
Adriel <adriel.fsantos@outlook.com>
2023-01-12 15:17:18 -0300

    Adiciona página explore #14

---
+++ src/pages/Explore/Explore.tsx
@@ -0,0 +1,50 @@
+import { useContext } from 'react';
+import { FiSettings } from 'react-icons/fi';
+import { NavLink } from 'react-router-dom';
+import AsideWhoToFollow from '../../components/AsideWhoToFollow';
+import Footer from '../../components/Footer';
+import Header from '../../components/Header';
+import SearchForm from '../../components/SearchForm';
+import SidebarWrapper from '../../components/SideBarWrapper';
+import AuthGoogleContext from '../../contexts/AuthGoogleContext';
+import * as S from './style';
+
+const Explore = () => {
+  const { user } = useContext(AuthGoogleContext);
+  return (
+    <S.Wrapper>
+      <Header />
+      <S.Container>
+        <S.TopSection>
+          <S.SearchWrapper>
+            <SearchForm />
+            <S.SettingsButtonWrapper>
+              <button>
+                <FiSettings />
+              </button>
+            </S.SettingsButtonWrapper>
+          </S.SearchWrapper>
+          <S.Navigation>
+            <NavLink to={'/explore'}>
+              <div>
+                <span>Assuntos do momento</span>
+                <div></div>
+              </div>
+            </NavLink>
+          </S.Navigation>
+        </S.TopSection>
+        <S.Trending sidebar={false} />
+        <S.Trending sidebar={false} />
+        <S.Trending sidebar={false} />
+        <S.Trending sidebar={false} />
+        <S.Trending sidebar={false} />
+      </S.Container>
+      <SidebarWrapper>
+        {user ? <AsideWhoToFollow /> : null}
+        <Footer />
+      </SidebarWrapper>
+    </S.Wrapper>
+  );
+};
+
+export default Explore;
