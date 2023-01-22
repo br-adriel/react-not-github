d45a0ca7ac094d131d4e5acd812075058897506a
Adriel <adriel.fsantos@outlook.com>
2023-01-22 19:46:18 -0300

    Implementa salvamento de dados do usuário na firestore

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,45 +1,83 @@
 import {
+  createUserWithEmailAndPassword,
   GoogleAuthProvider,
   signInWithEmailAndPassword,
   signInWithPopup,
   signOut,
+  updateProfile,
   User,
 } from 'firebase/auth';
-import { auth } from '../services/firebase';
+import { doc, setDoc } from 'firebase/firestore';
+import { auth, db } from '../services/firebase';
 import { store } from '../store';
 import { login, logout } from '../store/authSlice';
 
 const provider = new GoogleAuthProvider();
 
-export const signInGoogle = () => {
-  signInWithPopup(auth, provider)
-    .then((res) => {
-      const credential = GoogleAuthProvider.credentialFromResult(res);
-      const token = credential?.accessToken;
-      const user = res.user;
+export const signInGoogle = async () => {
+  try {
+    const res = await signInWithPopup(auth, provider);
+    const credential = GoogleAuthProvider.credentialFromResult(res);
+    const token = credential?.accessToken;
+    const user = res.user;
 
-      if (user && token) {
-        const userData = {
-          uid: user.uid,
-          photoURL: user.photoURL,
-          displayName: user.displayName,
-        };
+    if (user && token) {
+      const userData = {
+        uid: user.uid,
+        photoURL: user.photoURL,
+        displayName: user.displayName,
+        email: user.email,
+      };
 
-        sessionStorage.setItem('@AuthFirebase:user', JSON.stringify(userData));
+      sessionStorage.setItem('@AuthFirebase:user', JSON.stringify(userData));
 
-        store.dispatch(
-          login({
-            user: userData as User,
-          })
-        );
-      }
+      store.dispatch(
+        login({
+          user: userData as User,
+        })
+      );
+
+      const userRef = doc(db, 'users', userData.uid);
+      await setDoc(userRef, userData);
+    }
+  } catch (error) {
+    alert('Um erro ocorreu');
+  }
+};
+
+export const SignInWithEmail = async (
+  email: string,
+  name: string,
+  password: string
+) => {
+  const userEmail = email.trim();
+  const userName = name.trim();
+
+  const userCredential = await createUserWithEmailAndPassword(
+    auth,
+    userEmail,
+    password
+  );
+  await updateProfile(userCredential.user, {
+    displayName: userName,
+    photoURL: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
+  });
+
+  const userData = {
+    displayName: userCredential.user.displayName,
+    photoURL: userCredential.user.photoURL,
+    uid: userCredential.user.uid,
+    email: userCredential.user.email,
+  };
+
+  store.dispatch(
+    login({
+      user: userData as User,
     })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      const email = error.customData.email;
-      const credential = GoogleAuthProvider.credentialFromError(error);
-    });
+  );
+
+  const userRef = doc(db, 'users', userData.uid);
+  await setDoc(userRef, userData);
 };
 
 export const logInWithEmail = async (email: string, password: string) => {
